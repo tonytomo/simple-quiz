e8289635b98313bf323682f32107fb31eae4db73
tonytomo <fathonisatrioutomo@gmail.com>
2025-03-04 15:44:44 +0700

    update Svelte adapter configuration to use '404.html' as fallback page

--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
+			fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
