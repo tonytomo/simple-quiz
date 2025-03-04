b042a45bd4a34f1666fe200bac5efa355b91dd5d
tonytomo <fathonisatrioutomo@gmail.com>
2025-03-04 16:06:07 +0700

    remove static fallback page and update Svelte adapter configuration to use 'index.html' as fallback

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
